3f5d781f2e7d3995cb98fa1fa5c48255987e487d
hank.wang <eala0113@gmail.com>
2018-04-25 01:44:46 +0800

    add useful env setup script
    1. sample script is to build embedded build-tools
    2. including binutils, gcc, glibc

---
+++ envSetup.sh
@@ -0,0 +1,20 @@
+# script to setup environment of build_tools
+export PROJECT=build_tools
+export PROJECT_ROOT=/Users/hank/Desktop/Hank/Projects/src/${PROJECT}
+export TARGET=x86_64-apple-darwin17.5.0
+export PREFIX=${PROJECT_ROOT}/tools
+export TARGET_PREFIX=${PREFIX}/${TARGET}
+export PATH=${PREFIX}/bin:${PATH}
+
+# you may assign to native gnu gcc
+export CC=clang
+export CXX=clang++
+export CXX_FOR_TARGET=clang
+export CXX_FOR_TARGET=clang++
+
+cd ${PROJECT_ROOT}
+
+# build bin-utils
+./configure --target=${TARGET} --prefix=${PREFIX}
+make
+make install
